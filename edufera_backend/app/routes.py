73d7f398730d118ecb5cf305be308c05a58d0b83
kakou34 <kaouther.mouheb@gmail.com>
2021-04-25 15:49:14 +0300

    Dummy data added

--- edufera_backend/app/routes.py
+++ edufera_backend/app/routes.py
@@ -2,8 +2,8 @@ import itertools
 import operator
 from flask import request, make_response, jsonify
 from flask import current_app as app
-from .models import Meeting
-from edufera_backend.app.sample_data_generator import generate_data
+from .models import Meeting, Attendance
+from edufera_backend.app.sample_data_generator import generate_data, generate_attendances, generate_emotions
 
 
 @app.route('/start_meeting', methods=['GET'])
@@ -49,16 +49,25 @@ def past_meetings():
 @app.route('/attendances', methods=['GET'])
 def attendances():
     meeting_id = request.args.get('meeting_id')
-    if meeting_id:
-        meeting = Meeting.get_meeting(meeting_id)
-        if meeting:
-            return jsonify(meeting)
+    attendances = Attendance.get_attendance_by_meeting_id(meeting_id)
 
-    return make_response(f'Meeting: {meeting_id} does not exist!')
+    return jsonify(attendances)
 
 
 @app.route('/generate_data', methods=['GET'])
 def generate():
-    generate_data('C:/Users/99926527616etu/PycharmProjects/Thesis_EduFERA/edufera_backend/app/dummy_data.csv')
+    generate_data()
+    return make_response('done')
+
+
+@app.route('/generate_attendances', methods=['GET'])
+def generate_attend():
+    generate_attendances()
+    return make_response('done')
+
+
+@app.route('/generate_emotions', methods=['GET'])
+def generate_emo():
+    generate_emotions('C:/Users/99926527616etu/PycharmProjects/Thesis_EduFERA/edufera_backend/app/dummy_data.csv')
     return make_response('done')
 
--- edufera_backend/app/sample_data_generator.py
+++ edufera_backend/app/sample_data_generator.py
@@ -1,27 +1,80 @@
 from edufera_backend.app.models import Meeting, User, Attendance, Emotion
 from datetime import datetime as dt
 import pandas as pd
+from . import db
 
 
-def generate_data(path):
-    #meeting = Meeting.start_meeting("204 255 15", dt.strptime('18/09/19 01:00:00', '%d/%m/%y %H:%M:%S'))
+def generate_data():
+    meeting = Meeting.start_meeting("204 255 15", dt.strptime('18/09/19 01:00:00', '%d/%m/%y %H:%M:%S'))
     meeting = Meeting.end_meeting("204 255 15", dt.strptime('18/09/19 01:00:00', '%d/%m/%y %H:%M:%S'))
 
+    db.session.bulk_save_objects(
+        [
+            User(
+                user_id=f"{i+1}",
+                user_name=f"User{i+1}",
+            )
+            for i in range(25)
+        ],
+        return_defaults=True,
+    )
+    db.session.commit()
+
+
+def generate_attendances():
+    db.session.bulk_save_objects(
+        [
+            Attendance(
+                user_id=f"{i+1}",
+                meeting_id="204 255 15",
+                join_time= dt.strptime("Wed, 27 July 2016 13:30:00", '%a, %d %B %Y %H:%M:%S')
+            )
+            for i in range(18)
+        ],
+        return_defaults=True,
+    )
+    db.session.commit()
+
+    db.session.bulk_save_objects(
+        [
+            Attendance(
+                user_id=f"{i}",
+                meeting_id="204 255 15",
+                join_time=dt.strptime("Wed, 27 July 2016 13:30:01", '%a, %d %B %Y %H:%M:%S')
+            )
+            for i in [19, 20]
+        ],
+        return_defaults=True,
+    )
+    db.session.commit()
+
+    db.session.bulk_save_objects(
+        [
+            Attendance(
+                user_id=f"{i}",
+                meeting_id="204 255 15",
+                join_time=dt.strptime("Wed, 27 July 2016 13:30:02", '%a, %d %B %Y %H:%M:%S')
+            )
+            for i in [21, 22, 23, 24, 25]
+        ],
+        return_defaults=True,
+    )
+    db.session.commit()
+
+
+def generate_emotions(path):
     df = pd.read_csv(path)
-
-    for i, row in df.iterrows():
-        # Wed, 27 July 2016 13:30:00
-        time_stamp = dt.strptime(row[0], '%a, %d %B %Y %H:%M:%S')
-        user_id = row[1]
-        valence = row[2]
-        arousal = row[3]
-
-        user = User.get_user(user_id)
-        if user is None:
-            user = User.add_user(user_id)
-        attendance = Attendance.get_attendance_by_meeting_and_user(meeting.meeting_id, user_id)
-        if attendance is None:
-            attendance = Attendance.create_attendance(meeting.meeting_id, user_id, dt.now())
-        emotion = Emotion.save_emotion(meeting.meeting_id, user_id, valence, arousal, time_stamp)
-
-        print('done')
+    db.session.bulk_save_objects(
+        [
+            Emotion(
+                user_id=row[1],
+                meeting_id="204 255 15",
+                time_stamp=dt.strptime(row[0], '%a, %d %B %Y %H:%M:%S'),
+                valence=row[2],
+                arousal=row[3]
+            )
+            for i, row in df.iterrows()
+        ],
+        return_defaults=True,
+    )
+    db.session.commit()
