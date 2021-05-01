5afc9a286a728c6aa4f77502673d2991f9575a3d
kakou34 <kaouther.mouheb@gmail.com>
2021-05-01 03:33:35 +0300

    prediction functions done

--- edufera_backend/app/ml_model.py
+++ edufera_backend/app/ml_model.py
@@ -1,16 +1,12 @@
 import io
 import torch
-import numpy as np
 from facenet_pytorch import MTCNN
 from torchvision import transforms
 from PIL import Image
 from ml_models.resnet50_ferplus_dag import resnet50_ferplus_dag
 
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 model = resnet50_ferplus_dag('C:/Users/99926527616etu/PycharmProjects/Thesis_EduFERA/checkpoints/resnet50.pth')
-
 model.to(device)
 model.eval()
 
@@ -21,57 +17,57 @@ face_detector = MTCNN(image_size=48,
                       device=device, keep_all=False)
 
 
-def crop_image(image_bytes):
-    my_transforms = transforms.Compose([transforms.Grayscale(3), transforms.Resize(256), transforms.CenterCrop(224)])
-    image = Image.open(io.BytesIO(image_bytes))
-    return my_transforms(image)
+def fixed_image_standardization(image_tensor):
+    processed_tensor = (image_tensor - 127.5) / 128.0
+    return processed_tensor
 
 
 def transform_image(image):
-    my_transforms = transforms.Compose([
+    my_transforms = transforms.Compose([transforms.Grayscale(3),
                                         transforms.Resize(256),
                                         transforms.CenterCrop(224),
+                                        fixed_image_standardization
                                         ])
     return my_transforms(image).unsqueeze(0)
 
 
 def get_prediction(image_bytes):
-    image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(image_bytes)).resize((256, 256))
     face = face_detector(image)
     if face is None:
         return -1
     face = transform_image(face).to(device)
-    print(face)
     outputs = model.forward(face)
-    # _, y_hat = outputs.max(1)
-    # predicted_idx = y_hat.item()
-    return outputs.toList()
+    _, y_hat = outputs.max(1)
+    predicted_idx = y_hat.item()
+    return predicted_idx
 
 
 def batch_prediction(image_bytes_batch):
-    images = [crop_image(image_bytes) for image_bytes in image_bytes_batch]
+    size = len(image_bytes_batch)
+    results = [None] * size
+    images = [Image.open(io.BytesIO(image_bytes)).resize((256, 256))
+              for image_bytes in image_bytes_batch]
     faces = face_detector(images)
-    faces = [i for i in faces if i is not None]
-    faces = [transform_image(face) for face in faces]
-    inputs = torch.cat(faces).to(device)
-    outputs = model.forward(inputs)
+    for j in range(size):
+        if faces[j] is None:
+            results[j] = -1
+    faces = [transform_image(face) for face in faces if face is not None]
+    if len(faces) != 0:
+        inputs = torch.cat(faces).to(device)
+        outputs = model.forward(inputs)
+        _, y_hat = outputs.max(1)
+        predicted_ids = y_hat.tolist()
+        for j in range(size):
+            if results[j] is None:
+                results[j] = predicted_ids[0]
+                del predicted_ids[0]
 
-    _, y_hat = outputs.max(1)
-    predicted_ids = y_hat.tolist()
-    return predicted_ids
+    return results
 
 
 if __name__ == "__main__":
     image_bytes_batch = []
-
-    with open(r"giroud.jpg", 'rb') as f:
-        image_bytes = f.read()
-        image_bytes_batch.append(image_bytes)
-
-
-    with open(r"giroud.jpg", 'rb') as f:
-        image_bytes = f.read()
-        image_bytes_batch.append(image_bytes)
 
     with open(r"giroud.jpg", 'rb') as f:
         image_bytes = f.read()
@@ -85,8 +81,13 @@ if __name__ == "__main__":
         image_bytes = f.read()
         image_bytes_batch.append(image_bytes)
 
+    with open(r"giroud.jpg", 'rb') as f:
+        image_bytes = f.read()
+        image_bytes_batch.append(image_bytes)
+
+    with open(r"img.jpg", 'rb') as f:
+        image_bytes = f.read()
+        image_bytes_batch.append(image_bytes)
+
     out = batch_prediction(image_bytes_batch)
     print(out)
-
-
-
