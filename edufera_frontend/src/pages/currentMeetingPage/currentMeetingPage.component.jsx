ad952954a6555df34ec3cbfd340fddc877ab1f51
Kaouther Mouheb <kaouther.mouheb@gmail.com>
2021-05-11 07:05:35 +0300

    real-time meeting chart implemented

--- edufera_frontend/src/pages/currentMeetingPage/currentMeetingPage.component.jsx
+++ edufera_frontend/src/pages/currentMeetingPage/currentMeetingPage.component.jsx
@@ -2,17 +2,15 @@ import React, {useEffect, useState} from 'react';
 import './currentMeetingPage.syles.scss';
 import {socket} from '../../App'
 import axios from "axios";
-import {Line} from "react-chartjs-2";
+import {PolarArea} from "react-chartjs-2";
 
 
 const CurrentMeetingPage = (props) => {
     const roomId = props.match.params.meetingId // Gets roomId (meetingId) from URL
     const [status, setStatus] = useState('');
-    // const [labels, setLabels] = useState(['0', '1', '2']);
-    const [analysis, setAnalysis] = useState({
-        'data': [[1, 2, 3], [5, 0, 1], [0, 8, 2], [8, 9, 1], [0, 0, 5]],
-        'labels': ['0', '1', '2']
-    });
+    const [time, setTime] = useState('');
+    const [data, setData] = useState([0, 0, 0, 0, 0]);
+
     useEffect(() => {
         axios.get('http://localhost:5000/get_meeting_status', {params: {meeting_id: roomId}})
             .then(res => {
@@ -35,25 +33,10 @@ const CurrentMeetingPage = (props) => {
 
     useEffect(() => {
         socket.on('emotion_predicted', (resp) => {
-            console.log(resp.time_stamp + ': ' + resp.value);
-            const label = resp.time_stamp;
-            let value = resp.value;
-            // // const newLabels = labels;
-            if(value === -1) value = 4;
-            // // const i = newLabels.indexOf(label);
-            // // console.log(labels);
-            // // console.log(i);
-            // // if(i === -1){
-            // //     newLabels.push(label)
-            // // }
-            // // setLabels(newLabels);
-            setAnalysis(prevState => {
-                let newState = prevState;
-                let i = prevState.labels.indexOf(label)
-                if ( i === -1) {
-                    return [...prevState, label];
-                } else return prevState;
-            })
+            console.log('data received:');
+            console.log(resp);
+            setTime(resp.time_stamp);
+            setData(resp.results);
         });
     }, [])
 
@@ -62,80 +45,39 @@ const CurrentMeetingPage = (props) => {
         <div className='current'>
             <div className='meetingContainer'>
                 <div className='diagramCurrent-container'>
-                    <Line
-                        data={{
-                            labels: analysis.labels,
-                            datasets: [
-                                {
-                                    type: "line",
-                                    label: "Active Pleasant",
-                                    fill: false,
-                                    lineTension: 0.5,
-                                    backgroundColor: 'rgba(75,192,192,1)',
-                                    borderColor: 'rgba(0,0,0,1)',
-                                    borderWidth: 0.5,
-                                    data: analysis.data[0]
-                                }, {
-                                    type: "line",
-                                    label: "Active Unpleasant",
-                                    fill: false,
-                                    lineTension: 0.5,
-                                    backgroundColor: 'rgba(90,20,20,1)',
-                                    borderColor: 'rgba(0,0,0,1)',
-                                    borderWidth: 0.5,
-                                    data: analysis.data[1]
-                                }, {
-                                    type: "line",
-                                    label: "Inactive Unpleasant",
-                                    fill: false,
-                                    lineTension: 0.5,
-                                    backgroundColor: 'rgba(0,10,80,1)',
-                                    borderColor: 'rgba(0,0,0,1)',
-                                    borderWidth: 0.5,
-                                    data: analysis.data[2]
-                                }, {
-                                    type: "line",
-                                    label: "Inactive Pleasant",
-                                    fill: false,
-                                    lineTension: 0.5,
-                                    backgroundColor: 'rgba(0,150,0,1)',
-                                    borderColor: 'rgba(0,0,0,1)',
-                                    borderWidth: 0.5,
-                                    data: analysis.data[3]
-                                }, {
-                                    type: "line",
-                                    label: "No Face",
-                                    fill: false,
-                                    lineTension: 0.5,
-                                    backgroundColor: 'rgba(128,128,128,1)',
-                                    borderColor: 'rgba(0,0,0,1)',
-                                    borderWidth: 0.5,
-                                    data: analysis.data[4]
-                                }
-                            ]
-                        }}
-                        options={{
-                            responsive: true,
-                            maintainAspectRatio: false,
-                            tooltips: {
-                                enabled: true
-                            },
-                            scales: {
-                                xAxes: [
-                                    {
-                                        ticks: {
-                                            autoSkip: true,
-                                            maxTicksLimit: 10
-                                        }
-                                    }
-                                ]
-                            }
-                        }}/>
+                    <PolarArea style={{height: 500, width: 500}}
+                               data={
+                                   {
+                                       labels: ['Active Pleasant',
+                                           'Active Unpleasant',
+                                           'Inactive Unpleasant',
+                                           'Inactive Pleasant',
+                                           'No Face'],
+                                       datasets: [
+                                           {
+                                               label: time,
+                                               data: data,
+                                               backgroundColor: [
+                                                   'rgba(255, 99, 132, 0.8)',
+                                                   'rgba(54, 162, 235, 0.8)',
+                                                   'rgba(255, 206, 86, 0.8)',
+                                                   'rgba(75, 192, 192, 0.8)',
+                                                   'rgba(153, 102, 255, 0.6)',
+                                               ],
+                                               borderWidth: 1,
+                                           },
+                                       ],
+                                   }
+                               }
+                               options={{
+                                   animation: {
+                                       duration: 0
+                                   }
+                               }}
+                    />
                 </div>
                 <div className='txt-container'>
                     <p className='txt'>{status}</p>
-                    <p className='txt'>Online Students:</p>
-                    <p className='txt'>Offline Students:</p>
                 </div>
             </div>
         </div>
