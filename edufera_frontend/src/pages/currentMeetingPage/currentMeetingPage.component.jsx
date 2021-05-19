dbc2fbb2973ca8dcca4457de259a8099ab5eb1d0
Kaouther Mouheb <kaouther.mouheb@gmail.com>
2021-05-19 21:29:43 +0300

    polararea chart styling

--- edufera_frontend/src/pages/currentMeetingPage/currentMeetingPage.component.jsx
+++ edufera_frontend/src/pages/currentMeetingPage/currentMeetingPage.component.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './currentMeetingPage.syles.scss';
 import {socket} from '../../App'
 import axios from "axios";
-import {PolarArea} from "react-chartjs-2";
+import {PolarArea, Pie} from "react-chartjs-2";
 
 
 const CurrentMeetingPage = (props) => {
@@ -46,41 +46,43 @@ const CurrentMeetingPage = (props) => {
             <div className='meetingContainer'>
                 <div className='diagramCurrent-cont'>
                     <PolarArea
-                               data={
-                                   {
-                                       labels: ['Active Pleasant',
-                                           'Active Unpleasant',
-                                           'Inactive Unpleasant',
-                                           'Inactive Pleasant',
-                                           'No Face'],
-                                       datasets: [
-                                           {
-                                               label: time,
-                                               data: data,
-                                               backgroundColor: [
-                                                   'rgba(255, 99, 132, 0.8)',
-                                                   'rgba(54, 162, 235, 0.8)',
-                                                   'rgba(255, 206, 86, 0.8)',
-                                                   'rgba(75, 192, 192, 0.8)',
-                                                   'rgba(153, 102, 255, 0.6)',
-                                               ],
-                                               borderWidth: 1.5,
-                                           },
-                                       ],
-                                   }
-                               }
-                               options={{
-                                   animation: {
-                                       duration: 0
-                                   },
-                                 chart: {
-                    width: 200
-                  },
-                  legend: {
-                    position: 'bottom'
-                  }
+                        data={
+                            {
+                                labels: ['Active Pleasant',
+                                    'Active Unpleasant',
+                                    'Inactive Unpleasant',
+                                    'Inactive Pleasant',
+                                    'No Face'],
+                                datasets: [
+                                    {
+                                        label: time,
+                                        data: data,
+                                        backgroundColor: [
+                                            'rgba(255, 99, 132, 0.8)',
+                                            'rgba(54, 162, 235, 0.8)',
+                                            'rgba(255, 206, 86, 0.8)',
+                                            'rgba(75, 192, 192, 0.8)',
+                                            'rgba(153, 102, 255, 0.6)',
+                                        ],
+                                        borderWidth: 1.5,
+                                    },
+                                ],
+                            }
+                        }
+                        options={{
+                            animation: {
+                                duration: 0
+                            },
+                            chart: {
+                                width: 200
+                            },
+                            plugins: {
+                                legend: {
+                                    position: 'right'
+                                }
+                            }
 
-                               }}
+                        }}
                     />
                 </div>
                 <div className='txt-container'>
