1784b9f03f4c9e832dfaff364d9f753438944d80
Kaouther Mouheb <kaouther.mouheb@gmail.com>
2021-05-19 20:18:12 +0300

    import fixed

--- edufera_frontend/src/App.js
+++ edufera_frontend/src/App.js
@@ -2,10 +2,10 @@ import './App.css'
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 
-import PastMeetingsPage from './pages/PastMeetingsPage/pastMeetingsPage.component'
+import PastMeetingsPage from "./pages/PastMeetingsPage/pastMeetingsPage.component";
 import Header from './components/header/header.component'
-import UsersPage from "./pages/UsersPage/usersPage.component"
-import CurrentMeetingPage from './pages/CurrentMeetingPage/currentMeetingPage.component.jsx'
+import UsersPage from "./pages/UsersPage/usersPage.component";
+import CurrentMeetingPage from "./pages/CurrentMeetingPage/currentMeetingPage.component";
 import StartMeetingPage from './pages/StartMeetingPage/startMeetingPage.component'
 import UserList from './websocket'
 import io from 'socket.io-client'
